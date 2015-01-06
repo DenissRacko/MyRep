3e5c898ab748e08a56e742a425537d3efa327bde
Deniss <deniss@racko.org>
2015-01-06 13:23:24 +0200

    Novakts liekais teksts no aplikacijas

--- Test_Rep_app/Test_Rep_app/Program.cs
+++ Test_Rep_app/Test_Rep_app/Program.cs
@@ -9,24 +9,24 @@ namespace Test_Rep_app
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             int x, y, w;
-=======
-            int x, y, z;
->>>>>>> multiply
+
+            int z;
+
             string s;
             Console.WriteLine("Ievadiet x:");
             s = Console.ReadLine(); x = int.Parse(s);
             Console.WriteLine("Ievadiet y:");
             s = Console.ReadLine(); y = int.Parse(s);
-<<<<<<< HEAD
+
             w = x + y;
             Console.WriteLine("x un y summa ir: " + w.ToString());
-=======
+
             z = x * y;
             for (int i = 0; i <= z; i++)
                 Console.Write("*");
->>>>>>> multiply
+
             Console.ReadLine();
         }
     }
